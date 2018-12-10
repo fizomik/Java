9f4c257763aa5da4ffbe79b06e4d48b4a4164907
Damian Kozyra <fizomik@gmail.com>
2018-12-10 11:06:05 +0100

    lab9

---
+++ lab9/CommunicatorFx/src/sample/Client.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+public class Client extends NetworkConnection {
+
+    private String ip;
+    private int port;
+
+    public Client(String ip,int port, Consumer<Serializable> onReceiveCallback) {
+        super(onReceiveCallback);
+        this.ip=ip;
+        this.port=port;
+    }
+
+    @Override
+    protected boolean isServer() {
+        return false;
+    }
+
+    @Override
+    public String getIp() {
+        return ip;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+}
